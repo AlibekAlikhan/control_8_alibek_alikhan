3717e37ab8fe88e543e9f564a0efefde3207fb95
Alibek Alikhan <alialikhan0520@gmail.com>
2023-03-25 18:18:59 +0600

    Добавил функционалы CRUD

--- source/webapp/views/comment.py
+++ source/webapp/views/comment.py
@@ -1,50 +1,53 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView, ListView, TemplateView, UpdateView, DeleteView, FormView
+from django.views.generic import UpdateView, DeleteView, FormView
 
-from webapp.forms import ProjectForm
-from webapp.models import Project
+from webapp.forms import CommentForm
+from webapp.models import Comment
 
-from webapp.models import Task
+from webapp.models import Product
 
 
-class ProjectView(LoginRequiredMixin, FormView):
-    form_class = ProjectForm
+class CommentView(UserPassesTestMixin, LoginRequiredMixin, FormView):
+    form_class = CommentForm
 
     def post(self, request, *args, **kwargs):
-        product = get_object_or_404(Task, pk=kwargs.get('pk'))
+        product = get_object_or_404(Product, pk=kwargs.get('pk'))
         form = self.get_form_class()(request.POST)
         if form.is_valid():
             text_comment = form.cleaned_data.get('text_comment')
             grade = form.cleaned_data.get('grade')
             user = request.user
-            Project.objects.create(users=user, product=product, text_comment=text_comment, grade=grade)
+            Comment.objects.create(users=user, product=product, text_comment=text_comment, grade=grade)
         return redirect('index_article')
 
+    def test_func(self):
+        return self.request.user.groups.filter(name__in=['Moderator', 'users']).exists()
 
-class ProjectUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
-    model = Project
+
+class CommentUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = Comment
     template_name = 'update_comment.html'
-    form_class = ProjectForm
+    form_class = CommentForm
     context_key = 'project'
 
 
     def test_func(self):
-        return self.request.user.groups.filter(name__in=['admins']).exists()
+        return self.request.user.groups.filter(name__in=['Moderator', 'users']).exists()
 
     def get_success_url(self):
         return reverse_lazy('index_article')
 
 
-class ProjectDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+class CommentDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
     template_name = 'delit_comment.html'
-    model = Project
+    model = Comment
     context_object_name = 'project'
     success_url = reverse_lazy('index_article')
 
     def test_func(self):
-        return self.request.user.groups.filter(name__in=['admins']).exists()
+        return self.request.user.groups.filter(name__in=['Moderator', 'users']).exists()
 
     def get(self, request, *args, **kwargs):
         return self.delete(request)
--- source/webapp/views/product.py
+++ source/webapp/views/product.py
@@ -5,27 +5,27 @@ from django.urls import reverse, reverse_lazy
 from django.utils.http import urlencode
 from django.views.generic import TemplateView, ListView, CreateView, DeleteView, UpdateView
 from django.db.models import Q
-from webapp.forms import ArticleForm
+from webapp.forms import ProductForm
 
-from webapp.models import Task
+from webapp.models import Product
 
 from webapp.forms import SearchForm
 
-from webapp.forms import ProjectForm
+from webapp.forms import CommentForm
 
-from webapp.models import Project
+from webapp.models import Comment
 
 
-class ArticleView(ListView):
+class ProductView(ListView):
     template_name = "product.html"
-    model = Task
+    model = Product
     context_object_name = "tasks"
     ordering = ['-create_at']
-    paginate_by = 10
+    paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(object_list=object_list, **kwargs)
-        context['favorit_form'] = ProjectForm()
+        context['favorit_form'] = CommentForm()
         return context
 
     def get_queryset(self):
@@ -33,27 +33,27 @@ class ArticleView(ListView):
         return queryset.exclude(iis_deleted=True)
 
 
-class ArticleCreateView(UserPassesTestMixin, LoginRequiredMixin, CreateView):
+class ProductCreateView(UserPassesTestMixin, LoginRequiredMixin, CreateView):
     template_name = "product_create.html"
-    model = Task
-    form_class = ArticleForm
+    model = Product
+    form_class = ProductForm
 
     def test_func(self):
-        return self.request.user.groups.filter(name__in=['admins']).exists()
+        return self.request.user.groups.filter(name__in=['Moderator']).exists()
 
     def get_success_url(self):
         return reverse_lazy('detail_view', kwargs={'pk': self.object.pk})
 
 
-class ArticleUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
-    model = Task
+class ProductUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = Product
     template_name = "product_update.html"
-    form_class = ArticleForm
+    form_class = ProductForm
     context_object_name = 'task'
 
 
     def test_func(self):
-        return self.request.user.groups.filter(name__in=['admins']).exists()
+        return self.request.user.groups.filter(name__in=['Moderator']).exists()
 
 
 
@@ -61,25 +61,25 @@ class ArticleUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
         return reverse_lazy('detail_view', kwargs={'pk': self.object.pk})
 
 
-class ArticleDetailView(TemplateView):
+class ProductDetailView(TemplateView):
     template_name = "detail_product.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['task'] = get_object_or_404(Task, pk=kwargs['pk'])
-        comment = Q(product=get_object_or_404(Task, pk=kwargs['pk']))
-        context['comment'] = Project.objects.filter(comment)
+        context['task'] = get_object_or_404(Product, pk=kwargs['pk'])
+        comment = Q(product=get_object_or_404(Product, pk=kwargs['pk']))
+        context['comment'] = Comment.objects.filter(comment)
         return context
 
 
-class ArticleDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+class ProductDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
     template_name = 'delete_confirm.html'
-    model = Task
+    model = Product
     context_object_name = 'task'
     success_url = reverse_lazy('index_article')
 
     def test_func(self):
-        return self.request.user.groups.filter(name__in=['admins']).exists()
+        return self.request.user.groups.filter(name__in=['Moderator']).exists()
 
     def get(self, request, *args, **kwargs):
         return self.delete(request)
